1d071865c7df896b85c3b9af680c9b24be8c96f6
melisa-abuin <melisa.abuin@gmail.com>
2025-06-18 12:31:59 +0200

    feat(challenges): create start method

--- Source/Rewards/Challenges/ChainedTogether.cs
+++ Source/Rewards/Challenges/ChainedTogether.cs
@@ -7,5 +7,20 @@ public static class ChainedTogether
     // Evaluate if this will be a one time thing for particular people .. or an instanced type of object.. Perhaps change this to use OOP instead? 
 
 
+    /// <summary>
+    /// Starts the event, TBD how
+    /// </summary>
+    public static void StartEvent()
+    {
+        try
+        {
+            Utility.TimedTextToAllPlayers(4.0f, $"TOGHETHER WE STAND, DIVIDED WE FALL!");
+        }
 
+        catch (Exception e)
+        {
+            Logger.Warning($"Error in ChainedTogether.StartEvent {e.Message}");
+            throw;
+        }
+    }
 }
