d053c73e7e780f4b261072bba3c4b11b2806e86e
chase <chase.poland@gmail.com>
2025-06-17 23:59:29 -0400

    prep chained together class

---
+++ Source/Rewards/Challenges/ChainedTogether.cs
@@ -0,0 +1,11 @@
+﻿using WCSharp.Api;
+using static WCSharp.Api.Common;
+
+public static class ChainedTogether
+{
+
+    // Evaluate if this will be a one time thing for particular people .. or an instanced type of object.. Perhaps change this to use OOP instead? 
+
+
+
+}
