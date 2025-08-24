a0eb5966bf8a47ffc0cb5e14b009f94f71a0106d
chase <chase.poland@gmail.com>
2025-08-24 01:19:42 -0400

    Auto Item Pickup + less memory intensive

--- src/Game/Items/ItemSpatialGrid.ts
+++ src/Game/Items/ItemSpatialGrid.ts
@@ -2,74 +2,64 @@ import { Item } from 'w3ts'
 import { Kitty } from '../Entities/Kitty/Kitty'
 import { Kibble } from './Kibble'
 
-export class Cell {
-    public readonly x: number
-    public readonly y: number
-
-    public constructor(x: number, y: number) {
-        this.x = x
-        this.y = y
-    }
-}
-
 export class ItemSpatialGrid {
     private static CELL_SIZE = 128
-    private static kibbleCells: Map<Cell, Kibble[]> = new Map()
-    private static itemCells: Map<Cell, Item[]> = new Map()
+    private static kibbleCells: Map<string, Kibble[]> = new Map()
+    private static itemCells: Map<string, Item[]> = new Map()
 
-    public static GetCell(x: number, y: number): Cell {
-        const cellX = x / ItemSpatialGrid.CELL_SIZE
-        const cellY = y / ItemSpatialGrid.CELL_SIZE
-        return new Cell(cellX, cellY)
+    private static getCellKey(x: number, y: number): string {
+        const cellX = Math.floor(x / ItemSpatialGrid.CELL_SIZE)
+        const cellY = Math.floor(y / ItemSpatialGrid.CELL_SIZE)
+        return `${cellX},${cellY}`
     }
 
     public static RegisterKibble = (kibble: Kibble) => {
         if (!kibble.Item) return // If the item is not valid... no reason to register i guess
-        const cell = ItemSpatialGrid.GetCell(kibble.Item.x, kibble.Item.y)
-        let list = ItemSpatialGrid.kibbleCells.get(cell)
-        if (!list) ItemSpatialGrid.kibbleCells.set(cell, (list = []))
+        const cellKey = ItemSpatialGrid.getCellKey(kibble.Item.x, kibble.Item.y)
+        let list = ItemSpatialGrid.kibbleCells.get(cellKey)
+        if (!list) ItemSpatialGrid.kibbleCells.set(cellKey, (list = []))
         list.push(kibble)
     }
 
     public static UnregisterKibble = (kibble: Kibble) => {
         if (!kibble.Item) return // If the item is not valid... no reason to unregister i guess
-        const cell = ItemSpatialGrid.GetCell(kibble.Item.x, kibble.Item.y)
-        const list = ItemSpatialGrid.kibbleCells.get(cell) || []
+        const cellKey = ItemSpatialGrid.getCellKey(kibble.Item.x, kibble.Item.y)
+        const list = ItemSpatialGrid.kibbleCells.get(cellKey) || []
 
         ItemSpatialGrid.kibbleCells.set(
-            cell,
+            cellKey,
             list.filter(i => i !== kibble)
         )
     }
 
     public static RegisterItem = (item: Item) => {
-        const cell = ItemSpatialGrid.GetCell(item.x, item.y)
+        const cellKey = ItemSpatialGrid.getCellKey(item.x, item.y)
         let list: Item[]
-        if (!(list = ItemSpatialGrid.itemCells.get(cell)!)) ItemSpatialGrid.itemCells.set(cell, (list = []))
+        if (!(list = ItemSpatialGrid.itemCells.get(cellKey)!)) ItemSpatialGrid.itemCells.set(cellKey, (list = []))
         list.push(item)
     }
 
     public static UnregisterItem = (item: Item) => {
         if (!item) return // If the item is not valid... no reason to unregister i guess
-        const cell = ItemSpatialGrid.GetCell(item.x, item.y)
-        const list = ItemSpatialGrid.itemCells.get(cell) || []
+        const cellKey = ItemSpatialGrid.getCellKey(item.x, item.y)
+        const list = ItemSpatialGrid.itemCells.get(cellKey) || []
 
         ItemSpatialGrid.itemCells.set(
-            cell,
+            cellKey,
             list.filter(i => i !== item)
         )
     }
 
     public static GetNearbyKibbles = (x: number, y: number) => {
-        const cell = ItemSpatialGrid.GetCell(x, y)
-        const list = ItemSpatialGrid.kibbleCells.get(cell)
+        const cellKey = ItemSpatialGrid.getCellKey(x, y)
+        const list = ItemSpatialGrid.kibbleCells.get(cellKey)
         if (list) return list
         return null
     }
 
     public static GetNearbyItems = (x: number, y: number) => {
-        const cell = ItemSpatialGrid.GetCell(x, y)
-        const list = ItemSpatialGrid.itemCells.get(cell)
+        const cellKey = ItemSpatialGrid.getCellKey(x, y)
+        const list = ItemSpatialGrid.itemCells.get(cellKey)
         if (list) return list
         return null
     }
