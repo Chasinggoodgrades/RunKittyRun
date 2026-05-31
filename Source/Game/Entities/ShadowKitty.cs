3764af5d1f3db9ddf1418dd381d54c5138eeaafc
ChasingGoodGrades <chase.poland@gmail.com>
2026-05-31 00:52:11 -0400

    Fix #236 Fang of Shadows Shadow Kitty will now scale with your actual kitties collision reduction.

--- Source/Game/Entities/ShadowKitty.cs
+++ Source/Game/Entities/ShadowKitty.cs
@@ -51,6 +51,7 @@ public class ShadowKitty
         RelicUtil.CloseRelicBook(kitty);
         PauseKitty(this.Player, true);
         Utility.SelectUnitForPlayer(this.Player, this.Unit);
+        ShrinkShadowKitty();
         this.Active = true;
     }
 
@@ -104,5 +105,12 @@ public class ShadowKitty
     {
         this.wCollision = trigger.Create();
         this.cCollision = trigger.Create();
+    }
+
+    private void ShrinkShadowKitty()
+    {
+        if (this.Kitty.CurrentStats.CollisonRadius > CollisionDetection.DEFAULT_WOLF_COLLISION_RADIUS) return;
+        float scale = 0.60f - (0.60f * 0.10f * 2.0f);
+        this.Kitty.Unit.SetScale(scale, scale, scale);
     }
 }
