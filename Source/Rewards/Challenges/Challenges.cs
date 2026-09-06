d6349197157bd56efefae5f67a676acfd92872ed
ChasingGoodGrades <chase.poland@gmail.com>
2026-09-05 20:05:49 -0400

    Patriotic Light now goes based off round times instead of GAME TIMER.

--- Source/Rewards/Challenges/Challenges.cs
+++ Source/Rewards/Challenges/Challenges.cs
@@ -56,7 +56,7 @@ public static class Challenges
         if (!DifficultyConfig.IsVirtualRound(5, 5)) return;
         if (Difficulty.DifficultyValue == (int)DifficultyLevel.Progressive) return;
         if (!DifficultyConfig.MeetsDifficultyRequirement(DifficultyLevel.Impossible)) return;
-        if (Globals.GAME_TIMER.Remaining > 995) return; // Formally 20 mins, now 16:35 and awards to all players.
+        if (GameoverUtil.GetOverallGameTime() > 995) return; // Formally 20 mins, now 16:35 and awards to all players.
         AwardManager.GiveRewardAll(nameof(Globals.GAME_AWARDS_SORTED.Nitros.PatrioticLight));
     }
 
